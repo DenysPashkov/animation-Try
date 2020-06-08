bafbbe0797d448436287863668548043901342c6
dextronder97 <dexixoxa@yahoo.it>
2020-06-08 20:59:13 +0200

    .

--- animationTry/GameScene.swift
+++ animationTry/GameScene.swift
@@ -11,100 +11,71 @@ import GameplayKit
 
 class GameScene: SKScene {
     
-    var entities = [GKEntity]()
-    var graphs = [String : GKGraph]()
-    
-    private var lastUpdateTime : TimeInterval = 0
-    private var label : SKLabelNode?
-    private var spinnyNode : SKShapeNode?
+	var character : SKSpriteNode?
+//	Animation containers
+	var characterIdleFrames : [SKTexture] = []
+	var characterLeftFrames : [SKTexture] = []
     
     override func sceneDidLoad() {
-
-        self.lastUpdateTime = 0
-        
-        // Get label node from scene and store it for use later
-        self.label = self.childNode(withName: "//helloLabel") as? SKLabelNode
-        if let label = self.label {
-            label.alpha = 0.0
-            label.run(SKAction.fadeIn(withDuration: 2.0))
-        }
-        
-        // Create shape node to use during mouse interaction
-        let w = (self.size.width + self.size.height) * 0.05
-        self.spinnyNode = SKShapeNode.init(rectOf: CGSize.init(width: w, height: w), cornerRadius: w * 0.3)
-        
-        if let spinnyNode = self.spinnyNode {
-            spinnyNode.lineWidth = 2.5
-            
-            spinnyNode.run(SKAction.repeatForever(SKAction.rotate(byAngle: CGFloat(Double.pi), duration: 1)))
-            spinnyNode.run(SKAction.sequence([SKAction.wait(forDuration: 0.5),
-                                              SKAction.fadeOut(withDuration: 0.5),
-                                              SKAction.removeFromParent()]))
-        }
+//		take childnode character
+		if let player = childNode(withName: "character") as? SKSpriteNode{
+//			he is the player now
+			character = player
+//			for better pixel art presentation
+			character?.texture?.filteringMode = .nearest
+//			laddy way to add all the animation but not a good one
+///			Added this one just because it was a try for the animation and was the fastest way that i logically found
+			for i in 1...4{
+				let textureName = "swordsm_idle_lefthand_\(i)"
+				
+				let tempTexture = SKTexture(imageNamed: textureName)
+				tempTexture.filteringMode = .nearest
+				
+				characterIdleFrames.append(tempTexture)
+			}
+//			same as upstare
+			for i in 0...3{
+				let textureName = "swordsman-jump-50x30-0\(i)"
+				
+				let tempTexture = SKTexture(imageNamed: textureName)
+				tempTexture.filteringMode = .nearest
+				
+				characterLeftFrames.append(tempTexture)
+			}
+			
+		}
+		
+		animateCharacter()
+		
     }
+//	run the animation
+	func animateCharacter() {
+		character!.run(SKAction.repeatForever(
+		SKAction.animate(with: characterIdleFrames,
+						 timePerFrame: 0.2,
+						 resize: false,
+						 restore: true)),
+		withKey:"idleInPlaceBear")
+	}
+//	go animation 2
+	override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+		character?.removeAllActions()
+		character!.run(SKAction.repeatForever(
+		SKAction.animate(with: characterLeftFrames,
+						 timePerFrame: 0.1,
+						 resize: false,
+						 restore: true)),
+		withKey:"idleInPlaceBear")
+	}
+//	return animation 1
+	override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+		character?.removeAllActions()
+		character!.run(SKAction.repeatForever(
+		SKAction.animate(with: characterIdleFrames,
+						 timePerFrame: 0.2,
+						 resize: false,
+						 restore: true)),
+		withKey:"idleInPlaceBear")
+	}
     
-    
-    func touchDown(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.green
-            self.addChild(n)
-        }
-    }
-    
-    func touchMoved(toPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.blue
-            self.addChild(n)
-        }
-    }
-    
-    func touchUp(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.red
-            self.addChild(n)
-        }
-    }
-    
-    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        if let label = self.label {
-            label.run(SKAction.init(named: "Pulse")!, withKey: "fadeInOut")
-        }
-        
-        for t in touches { self.touchDown(atPoint: t.location(in: self)) }
-    }
-    
-    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchMoved(toPoint: t.location(in: self)) }
-    }
-    
-    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
-    }
-    
-    override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
-    }
-    
-    
-    override func update(_ currentTime: TimeInterval) {
-        // Called before each frame is rendered
-        
-        // Initialize _lastUpdateTime if it has not already been
-        if (self.lastUpdateTime == 0) {
-            self.lastUpdateTime = currentTime
-        }
-        
-        // Calculate time since last update
-        let dt = currentTime - self.lastUpdateTime
-        
-        // Update entities
-        for entity in self.entities {
-            entity.update(deltaTime: dt)
-        }
-        
-        self.lastUpdateTime = currentTime
-    }
 }
